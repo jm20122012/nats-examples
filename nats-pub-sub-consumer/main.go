09eb85dafa00128e3934fedae2828564b48acc7c
jm20122012 <merritt.josh2012@protonmail.com>
2024-09-21 20:54:59 -0600

    Updated producer and consumer to test throughput

--- nats-pub-sub-consumer/main.go
+++ nats-pub-sub-consumer/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"errors"
 	"log/slog"
 	"os"
-	"time"
+	"os/signal"
+	"syscall"
 
 	"github.com/nats-io/nats.go"
 )
 
 const (
 	NATS_SERVER_URL string = "192.168.3.34:4222"
+)
+
+var (
+	totalMsgcout int
 )
 
 // A sync subscriber requires the consumer to use NextMsg to read the next message from the buffer.
@@ -26,7 +30,8 @@ func createSyncSubscriber(nc *nats.Conn, subject string) (*nats.Subscription, er
 
 // An async subscriber consumes the data from the subject as soon as it's available.
 func createAsyncSubscriber(nc *nats.Conn, subject string) (*nats.Subscription, error) {
-	handler := subMsgHandlerFactory(subject)
+	// handler := subMsgHandlerFactory(subject)
+	handler := subMsgHandlerFactory()
 	sub, err := nc.Subscribe(subject, handler)
 	if err != nil {
 		return &nats.Subscription{}, err
@@ -34,13 +39,30 @@ func createAsyncSubscriber(nc *nats.Conn, subject string) (*nats.Subscription, e
 	return sub, nil
 }
 
-func subMsgHandlerFactory(subject string) nats.MsgHandler {
+func subMsgHandlerFactory() nats.MsgHandler {
 	return func(msg *nats.Msg) {
-		slog.Info("Message received", "subject", subject, "msg", msg.Data)
+		// slog.Info("Message received", "subject", subject, "msg", msg.Data)
+		totalMsgcout++
 	}
 }
 
 func main() {
+	// Create a channel to receive OS signals
+	sigs := make(chan os.Signal, 1)
+
+	// Register the channel to receive SIGINT signals
+	signal.Notify(sigs, syscall.SIGINT)
+
+	// Start a goroutine to handle the signal
+	go func() {
+		sig := <-sigs
+		slog.Info("Signal received", "signal", sig)
+
+		// Perform any cleanup here
+		slog.Info("Exiting", "totalMessagesReceived", totalMsgcout)
+		os.Exit(0)
+	}()
+
 	nc, err := nats.Connect(NATS_SERVER_URL)
 	if err != nil {
 		slog.Error("error connecting to NATS server", "error", err)
@@ -50,42 +72,44 @@ func main() {
 
 	slog.Info("Connected to NATS server", "serverURL", NATS_SERVER_URL)
 
-	slog.Info("Creating sync consumer")
-	syncSub, err := createSyncSubscriber(nc, "producer-0.>")
-	if err != nil {
-		slog.Error("Error creating sync subscriber", "error", err)
-	}
-	defer syncSub.Drain()
+	// slog.Info("Creating sync consumer")
+	// syncSub, err := createSyncSubscriber(nc, "producer-0.>")
+	// if err != nil {
+	// 	slog.Error("Error creating sync subscriber", "error", err)
+	// }
+	// defer syncSub.Drain()
 
 	slog.Info("Creating async consumer")
-	sub, err := createAsyncSubscriber(nc, "producer-1.>")
+	sub, err := createAsyncSubscriber(nc, "trace_data.>")
 	if err != nil {
 		slog.Error("Error creating async subscriber", "error", err)
 	}
 	defer sub.Drain()
 
-	ticker := time.NewTicker(time.Duration(1 * time.Second))
-	defer ticker.Stop()
+	<-sigs
 
-	timer := time.NewTimer(time.Duration(60 * time.Second))
-	defer timer.Stop()
+	// ticker := time.NewTicker(time.Duration(1 * time.Second))
+	// defer ticker.Stop()
 
-	for {
-		select {
-		case <-ticker.C:
-			m, err := syncSub.NextMsg(time.Duration(5 * time.Second))
-			if err != nil {
-				if errors.Is(err, nats.ErrTimeout) {
-					slog.Info("No messages in buffer for sync sub")
-				} else {
-					slog.Error("Could not get next message for sync sub", "error", err)
-				}
-				continue
-			}
-			slog.Info("New message on sync sub", "msg", m.Data)
-		case <-timer.C:
-			slog.Info("Timer expired - exiting main")
-			return
-		}
-	}
+	// timer := time.NewTimer(time.Duration(60 * time.Second))
+	// defer timer.Stop()
+	// <-timer.C
+	// for {
+	// 	select {
+	// 	case <-ticker.C:
+	// 		m, err := syncSub.NextMsg(time.Duration(5 * time.Second))
+	// 		if err != nil {
+	// 			if errors.Is(err, nats.ErrTimeout) {
+	// 				slog.Info("No messages in buffer for sync sub")
+	// 			} else {
+	// 				slog.Error("Could not get next message for sync sub", "error", err)
+	// 			}
+	// 			continue
+	// 		}
+	// 		slog.Info("New message on sync sub", "msg", m.Data)
+	// 	case <-timer.C:
+	// 		slog.Info("Timer expired - exiting main")
+	// 		return
+	// 	}
+	// }
 }
--- nats-pub-sub-producer/main.go
+++ nats-pub-sub-producer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log/slog"
 	"math/rand"
@@ -16,9 +18,15 @@ const (
 	PRODUCER_COUNT  int    = 2
 )
 
-func publishMessages(wg *sync.WaitGroup, id int, nc *nats.Conn, subject string, msgCount int, interval time.Duration) {
+type IPublisher interface {
+	publishMessages(wg *sync.WaitGroup, id int, nc *nats.Conn, subject string, msgCount int, interval time.Duration)
+}
+
+type SimpleTestPublisher struct{}
+
+func (p *SimpleTestPublisher) publishMessages(wg *sync.WaitGroup, id int, nc *nats.Conn, subject string, msgCount int, interval time.Duration) {
 	defer wg.Done()
-	slog.Info("Starting publisher go routine", "id", id, "msgCount", msgCount, "interval", interval)
+	slog.Info("Starting simple publisher go routine", "id", id, "msgCount", msgCount, "interval", interval)
 
 	for i := 0; i < msgCount; i++ {
 		msg := fmt.Sprintf("msg number: %d", i)
@@ -33,6 +41,40 @@ func publishMessages(wg *sync.WaitGroup, id int, nc *nats.Conn, subject string, 
 	slog.Info("Publisher go routine finished", "id", id, "msgCount", msgCount, "interval", interval)
 }
 
+type Publisher struct{}
+
+func (p *Publisher) publishMessages(wg *sync.WaitGroup, id int, nc *nats.Conn, subject string, msgCount int, interval time.Duration) {
+	defer wg.Done()
+
+	slog.Info("Starting publisher go routine", "id", id, "msgCount", msgCount, "interval", interval)
+
+	data := make([]float64, 100)
+
+	for i := 0; i < msgCount; i++ {
+		data = data[:0]
+		for j := 0; j < 100; j++ {
+			randMeasurement := rand.Float64()*10 - 80 // Generate a random float between -80 and -70
+			data = append(data, randMeasurement)
+		}
+
+		encodedData, err := encodeFloat64Array(data)
+		if err != nil {
+			slog.Error("Error encoding data to binary in publisher", "id", id, "subject", subject, "error", err)
+			continue
+		}
+
+		err = nc.Publish(subject, encodedData)
+		if err != nil {
+			slog.Error("Error publishing data to NATS", "id", id, "subject", subject, "error", err)
+			continue
+		}
+
+		// slog.Info("Successully published message", "id", id, "subject", subject)
+		time.Sleep(interval)
+	}
+
+}
+
 func main() {
 	nc, err := nats.Connect(NATS_SERVER_URL)
 	if err != nil {
@@ -42,16 +84,47 @@ func main() {
 	defer nc.Drain()
 
 	slog.Info("Connected to NATS server", "serverURL", NATS_SERVER_URL)
-
 	var wg sync.WaitGroup
-	for i := 0; i < PRODUCER_COUNT; i++ {
-		wg.Add(1)
-		subj := fmt.Sprintf("producer-%d.msgs", i)
-		pubInterval := time.Duration(rand.Intn(1000)) * time.Millisecond
-		go publishMessages(&wg, i, nc, subj, rand.Intn(100), pubInterval)
-	}
+
+	// sp := SimpleTestPublisher{}
+	// for i := 0; i < PRODUCER_COUNT; i++ {
+	// 	wg.Add(1)
+	// 	subj := fmt.Sprintf("producer-%d.msgs", i)
+	// 	pubInterval := time.Duration(rand.Intn(1000)) * time.Millisecond
+	// 	go sp.publishMessages(&wg, i, nc, subj, rand.Intn(100), pubInterval)
+	// }
+
+	sp := newPublisher("normal")
+	id := 0
+	subj := "trace_data.spec-a-1"
+	pubInterval := time.Millisecond * 1
+	msgCount := 1_000
+	wg.Add(1)
+	go sp.publishMessages(&wg, id, nc, subj, msgCount, pubInterval)
 
 	slog.Info("Waiting for publishers to complete work...")
 	wg.Wait()
 	slog.Info("All publisher work completed")
+}
+
+func encodeFloat64Array(data []float64) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	for _, v := range data {
+		err := binary.Write(buf, binary.LittleEndian, v)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buf.Bytes(), nil
+}
+
+func newPublisher(pubType string) IPublisher {
+	switch pubType {
+	case "simple":
+		return &SimpleTestPublisher{}
+	case "normal":
+		return &Publisher{}
+	default:
+		return &Publisher{}
+	}
 }
